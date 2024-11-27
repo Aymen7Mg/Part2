f2a840524051e6087605bdd697a24056a2ceaf52
Aymen7Mg <maatougaymane@gmail.com>
2024-11-27 20:42:33 +0100

    Add files via upload

---
+++ Lecture.java
@@ -0,0 +1,21 @@
+package part2;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LectureText {
+    public static void main(String[] args) {
+        String fichierTexte = "livres.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fichierTexte))) {
+            String ligne;
+            // Read and print each line of the text file
+            while ((ligne = br.readLine()) != null) {
+                System.out.println(ligne);
+            }
+        } catch (IOException e) {
+            System.err.println("Erreur lors de la lecture du fichier : " + e.getMessage());
+        }
+    }
+}
